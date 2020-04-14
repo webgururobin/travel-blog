772fdde2c0d1857d3240c042bf3625a1d38e4347
Robin Solanki <web.guru@me.com>
2020-04-14 10:55:14 +0530

    Configure gatsby create node to get blog posts and create page from contenful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,47 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  // Query for markdown nodes to use in creating pages
+  const blogPostResult = await graphql(
+    `
+      {
+        allContentfulBlog(
+          filter: { node_locale: { eq: "en-US" } }
+          sort: { fields: createdAt, order: DESC }
+        ) {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+
+  // Handle errors
+  if (blogPostResult.errors) {
+    reporter.panicOnBuild(`Error While running GraphQL query.`)
+    return
+  }
+
+  // Create Pages for each markdown file
+  const blogPostTemplate = path.resolve(`src/templates/blog.js`)
+
+  blogPostResult.data.allContentfulBlog.edges.forEach(({ node }) => {
+    const path = `blog/${node.slug}`
+
+    createPage({
+      path,
+      component: blogPostTemplate,
+      // In your blog post template's graphql query, you can use pagePath
+      // as a GraphQL variable to query for data from the markdown file.
+      context: {
+        pagePath: path,
+      },
+    })
+  })
+}
---
+++ src/templates/blog.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const blogTemplate = () => {
+  return <div>Blog</div>
+}
+
+export default blogTemplate
